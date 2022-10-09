a162e934075e3cd12b5c9c673829bf559727ad0c
alexandermehks <alexandermehks@gmail.com>
2022-10-09 16:23:28 +0200

    update

--- src/network/request.rs
+++ src/network/request.rs
@@ -5,24 +5,21 @@ let resp = reqwest::get(url)
         .await?
         .text()
         .await?;
-
-    let _keys = fetch_keys(&resp);
-
+    let keys = fetch_keys(&resp) 
 
     Ok(())
 
 }
 
-
-pub fn fetch_keys(text: &str){
+pub fn fetch_keys(text: &str) -> Vec<String>{
+    let mut stack = Vec::new();
     let splitted: Vec<&str> = text.split("{").collect();
     let splitted2: Vec<&str> = splitted[splitted.len()-2].split(":").collect();
     let re = Regex::new(r"\b(\w+)\W*$").expect("No matches for regex");
     for row in splitted2{
-        let hehe = re.find(row).unwrap();
-        let luls = hehe.as_str().replace('"', "").replace("}", "").replace(",","").replace('\n', "").replace(" ", "");
-        dbg!(luls);
-
-
+        let regex_match = re.find(row).unwrap();
+        let replacing_non_chars = regex_match.as_str().replace('"', "").replace("}", "").replace(",","").replace('\n', "").replace(" ", "");
+        stack.push(replacing_non_chars);
     }
+    stack
 }
