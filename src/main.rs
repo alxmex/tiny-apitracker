f89f5e00e04400431f25a5c631d9c263a96333cd
Alexander Mehks <54999803+art1zt@users.noreply.github.com>
2022-10-10 12:59:30 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,5 @@ use crate::network::request::*;
 #[tokio::main]
 async fn main() {
     let response: Vec<String> = send_request("https://jsonplaceholder.typicode.com/posts").await.expect("Endpoint cant be called");
-
     println!("Response: {:?} acquired.", response);
 }
