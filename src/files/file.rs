51aea68c11c15e9d0a28c07a25228e254d7d9669
alexandermehks <alexandermehks@gmail.com>
2022-10-16 16:37:21 +0200

    update

--- src/files/file.rs
+++ src/files/file.rs
@@ -2,7 +2,8 @@ use std::fs::File;
 use std::io::prelude::*;
 
 pub fn write_file(response: &Vec<String>, endpoint: &str) -> Result<(), Box<dyn std::error::Error>>{
-    let mut file = File::options().append(true).open("response.txt")?;
+    //TODO: Fix the path to scale on any machine.
+    let mut file = File::options().append(true).open("/home/anorak/.rcheck/responses.txt")?;
     let string_to_write = format!("\n{}", endpoint);
     file.write_all(string_to_write.as_bytes())?;
     for key in response{
--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,6 @@ async fn main() {
     let response: Vec<String> = network::request::send_request(&end_point).await.expect("Endpoint cant be called");
 
     //Store response 
-    
     let end_point = files::file::strip_dot(end_point);
     let write = files::file::write_file(&response, &end_point);
     match write {
