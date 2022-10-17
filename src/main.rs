a8abbed66628bba0b055f7ff99d512f6365bbe63
alexandermehks <alexandermehks@gmail.com>
2022-10-17 21:09:49 +0200

    update

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,25 @@
 mod network;
 mod files;
 
+use std::fs;
+
+use serde_derive::Deserialize;
+
+#[derive(Deserialize)]
+struct EndPoint {
+    url: String,
+    auth: String
+}
+
 #[tokio::main]
 async fn main() {
-    let end_point = "https://jsonplaceholder.typicode.com/posts".to_string();
+    let endpoints_file = fs::read_to_string("endpoints.json").expect("HEH");
+    let ep: EndPoint = serde_json::from_str(&endpoints_file).unwrap();
+
+    let end_point = ep.url.to_string();
     let response: Vec<String> = network::request::send_request(&end_point).await.expect("Endpoint cant be called");
 
-    //Store response 
+    ////Store response 
     let end_point = files::file::strip_dot(end_point);
     let write = files::file::write_file(&response, &end_point);
     match write {
